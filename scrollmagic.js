8b09111026c3aef62292a5e5077cf745afd52fad
Borey-PC <buttscl@mail.uc.edu>
2018-06-12 20:45:18 -0400

    Got scrolling animations to work

--- scrollmagic.js
+++ scrollmagic.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
     var controller = new ScrollMagic.Controller();
 
+    var tween0 = TweenMax.to("body", 0.5, {
+        backgroundColor: "white"
+    })
+
     var scene = new ScrollMagic.Scene({
-        triggerElement: '#page0',
+        triggerElement: "#trigger0",
         duration: 300
     })
-    .setClassToggle('#page0', 'fade-in')
+    .setTween(tween0)
+    .setPin("#pin0")
+    .addIndicators({name: "2 (duration: 0)"})
     .addTo(controller);
 });
