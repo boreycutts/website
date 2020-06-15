91a763c0d63d67e07812f795b0c34c1d5553f58e
Borey <Borey@fvfyt39bhv2h.home>
2020-06-15 15:04:19 -0400

    Fixing skill container scaling on hover

--- client/components/skillCard/skillCard.js
+++ client/components/skillCard/skillCard.js
@@ -2,6 +2,15 @@ import WebComponent from '/components/webComponent.js';
 
 export default class SkillCard extends WebComponent {
     expandContainer() {
-        console.log('hello')
+        console.log((window.innerWidth - 100) % 110);
+        if((window.innerWidth - 100) % 110 < 48 && (window.innerWidth - 100) % 110 > 0) {
+            const container = document.getElementById('skills-container');
+            container.style.width = 'calc(100% + 50px)';
+        }
+    }
+    
+    collapseContainer() {
+        const container = document.getElementById('skills-container');
+        container.style.width = '100%';
     }
 }
